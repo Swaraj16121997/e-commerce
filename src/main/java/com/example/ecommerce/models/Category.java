45aab7e149bb5359309e344481a2da9f54893f07
Swaraj <swaraj@nair.com>
2024-02-02 00:01:28 +0530

    Fetch mode, N+1 problem and Writing JPA query methods

--- src/main/java/com/example/ecommerce/models/Category.java
+++ src/main/java/com/example/ecommerce/models/Category.java
@@ -2,9 +2,12 @@ package com.example.ecommerce.models;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class Category extends BaseModel{
     private String name;
     private String description;
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Fetch(FetchMode.JOIN)
     private List<Product> productList;
 }

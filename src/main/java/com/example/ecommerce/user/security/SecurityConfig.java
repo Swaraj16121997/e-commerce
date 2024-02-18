68a1f64ddd9ff093f11aa93cda8f1dfcc4f5711c
Swaraj <swaraj@nair.com>
2024-02-18 19:46:43 +0530

    added custom authority converter

--- src/main/java/com/example/ecommerce/user/security/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/user/security/SecurityConfig.java
@@ -4,7 +4,10 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Collections;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -21,6 +24,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,6 +33,7 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -36,6 +41,8 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -99,7 +106,12 @@ public class SecurityConfig {   // template of this class is available in the "s
 //                .authorizeHttpRequests((authorize) -> authorize
 //                        .requestMatchers("/products/*")
 //                        .hasAuthority("Admin")
-//                        .anyRequest().permitAll());
+//                        .anyRequest().permitAll())
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(
+//                        jwtConfigurer -> {
+//                            jwtConfigurer.jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
+//                        }
+//                ));
 
         return http.build();
     }
@@ -170,4 +182,19 @@ public class SecurityConfig {   // template of this class is available in the "s
         return AuthorizationServerSettings.builder().build();
     }
 
+//    @Bean
+//    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+//        return (context) -> {
+//            if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+//                context.getClaims().claims((claims) -> {
+//                    Set<String> roles = AuthorityUtils.authorityListToSet(context.getPrincipal().getAuthorities())
+//                            .stream()
+//                            .map(c -> c.replaceFirst("^ROLE_", ""))
+//                            .collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
+//
+//                    claims.put("roles", roles);
+//                });
+//            }
+//        };
+//    }
 }
--- src/test/java/com/example/ecommerce/user/security/repository/JpaRegisteredClientRepositoryTest.java
+++ src/test/java/com/example/ecommerce/user/security/repository/JpaRegisteredClientRepositoryTest.java
@@ -1,45 +1,45 @@
-package com.example.ecommerce.user.security.repository;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.test.annotation.Commit;
-
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class JpaRegisteredClientRepositoryTest {
-    @Autowired
-    private RegisteredClientRepository registeredClientRepository;
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Test
-    @Commit
-    void addNewRegisteredClient() {         // the purpose of writing this test case is just to register the client from the in-memory storage to our jpa storage or database
-        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("client")
-                .clientSecret(bCryptPasswordEncoder.encode("secret"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
-
-        registeredClientRepository.save(oidcClient);
-    }
-}
+//package com.example.ecommerce.user.security.repository;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.oauth2.core.AuthorizationGrantType;
+//import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+//import org.springframework.security.oauth2.core.oidc.OidcScopes;
+//import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+//import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+//import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+//import org.springframework.test.annotation.Commit;
+//
+//import java.util.UUID;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class JpaRegisteredClientRepositoryTest {
+//    @Autowired
+//    private RegisteredClientRepository registeredClientRepository;
+//
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    @Test
+//    @Commit
+//    void addNewRegisteredClient() {         // the purpose of writing this test case is just to register the client from the in-memory storage to our jpa storage or database
+//        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//                .clientId("client")
+//                .clientSecret(bCryptPasswordEncoder.encode("secret"))
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+//                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+//                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+//                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.PROFILE)
+//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//                .build();
+//
+//        registeredClientRepository.save(oidcClient);
+//    }
+//}
